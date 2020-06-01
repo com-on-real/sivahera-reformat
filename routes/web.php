b7752e1aa1d8167e8f6229a6bc864032e6278c69
root <root@vps.comon-real.fr>
2020-06-01 16:23:31 +0200

    change routes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 Route::name('language')->get('/{lang}', 'SinglePageController@language');
 
 
-Route::domain('cafe.sivahera.local')->group(function ()
+Route::domain('cafe.sivahera.com')->group(function ()
 {
     Route::get('/', 'SinglePageController@coffee')->name('coffee_path');
 });
 
-Route::domain('management.sivahera.local')->group(function ()
+Route::domain('management.sivahera.com')->group(function ()
 {
     Route::get('/', 'SinglePageController@management')->name('home_path');
     Route::get('/courtage', 'SinglePageController@courtage')->name('courtage_path');
@@ -16,7 +16,9 @@ Route::domain('management.sivahera.local')->group(function ()
 });
 
 
-
+/**
 Route::get('/', function () {
     return redirect('//management.sivahera.local');
 });
+
+*:
